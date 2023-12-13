9b0458eb65bb6da1119bb2e38b58ed1a7e032745
nextokoi <nextokoi.dev@gmail.com>
2023-12-13 15:11:10 +0000

    landing page and footer basic styled

--- itinerario-frontend/src/pages/LandingPage.jsx
+++ itinerario-frontend/src/pages/LandingPage.jsx
@@ -1,8 +1,11 @@
-import { Box, Button } from "@mui/material"
+import { Box, Button, Typography } from "@mui/material"
 import { Link } from 'react-router-dom'
 import SearchBar from "../components/SearchBar/SearchBar"
 import { useState, useEffect, useContext } from "react"
 import { countryContext } from "../contexts/countryContext"
+import PublicIcon from '@mui/icons-material/Public';
+import HikingIcon from '@mui/icons-material/Hiking';
+import FactCheckIcon from '@mui/icons-material/FactCheck';
 
 function LandingPage() {
 
@@ -30,6 +33,7 @@ function LandingPage() {
 
   }
 
+  const heroImage = './../../public/photos/hero-image.jpg'
 
   return (
 
@@ -47,9 +51,31 @@ function LandingPage() {
         />
 
         <Link to='/about'><Button variant="contained" sx={{ backgroundColor: 'purple', padding: '15px' }}>Search</Button></Link>
-
       </Box>
-      <div>LandingPage</div>
+      <Box sx={{display: 'flex', flexDirection: 'column', alignItems: 'center', gap: '100px', paddingY: '100px'}}>
+        <Box sx={{display: 'flex', gap: '50px', justifyContent: 'center', width: '80%'}}>
+          <Box sx={{display: 'flex', gap: '10px'}}>
+            <PublicIcon /><Typography>Discover your ideal destination for the perfect getaway</Typography>
+          </Box> 
+
+          <Box sx={{display: 'flex', gap: '10px'}}>
+            <HikingIcon /> <Typography> Choose from a variety of activities to tailor your travel experience to your liking</Typography>
+          </Box>
+
+          <Box sx={{display: 'flex', gap: '10px'}}>
+            <FactCheckIcon /> <Typography>Save a summary of your itinerary with all your chosen activities to your profile for easy access and sharing</Typography>
+          </Box>
+        </Box>
+        <Box sx={{backgroundImage: `url(${heroImage})`, height: '767px', width: '80%', backgroundSize: 'cover', backgroundPosition: 'center'}}>
+          <Box sx={{display: 'flex', flexDirection: 'column', alignItems: 'start', height: '100%', justifyContent: 'space-around', paddingLeft: '20px'}}>
+            <Box width={300}>
+              <Typography variant="h6">What will be your next destination?</Typography>
+              <Typography variant="h4">The world is full of mysteries</Typography>
+            </Box>
+            <Button variant="contained" sx={{padding: '20px'}}>Start your journey</Button>
+          </Box>
+        </Box>
+      </Box>
     </Box>
   )
 }
