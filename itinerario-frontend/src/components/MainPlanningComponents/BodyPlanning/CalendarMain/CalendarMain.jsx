acc0de302eaf85b8de6b45ac255f677aa695ee0d
Oscar <oscarlp393@gmail.com>
2023-12-15 16:09:29 +0000

    refactorized Prop Button-Stepper navigation

--- itinerario-frontend/src/components/MainPlanningComponents/BodyPlanning/CalendarMain/CalendarMain.jsx
+++ itinerario-frontend/src/components/MainPlanningComponents/BodyPlanning/CalendarMain/CalendarMain.jsx
@@ -2,30 +2,32 @@ import { Box } from '@mui/material'
 import ResponsiveDatePickers from '../../../GenericComponents/CalendarPlanning/CalendarPlanning.jsx'
 import ButtonNavigation from '../../../GenericComponents/ButtonNavigation/ButtonNavigation.jsx'
 
-export default function CalendarComponent({handleNavigation}) {
+export default function CalendarComponent({ handleNavigation }) {
     return (
-    <>
-        <Box sx={{
-            alignSelf: "center",
-            width: "500px",
-            height: "200px",
-            marginY: "50px",
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            gap: "30px",
-            borderRadius: "10px",
-            border: "1px solid #1976d2"
-        }} >
+        <>
+            <Box sx={{
+                alignSelf: "center",
+                width: "500px",
+                height: "200px",
+                marginY: "50px",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                gap: "30px",
+                borderRadius: "10px",
+                border: "1px solid #1976d2"
+            }} >
 
-            <ResponsiveDatePickers labelText={"ida"} />
-            <ResponsiveDatePickers labelText={"vuelta"} />
+                <ResponsiveDatePickers labelText={"ida"} />
+                <ResponsiveDatePickers labelText={"vuelta"} />
 
-        </Box>
-           <Box sx={{display: 'flex', justifyContent: 'space-around'}}>
+            </Box>
+
+            {/* <Box sx={{display: 'flex', justifyContent: 'space-around'}}>
                 <ButtonNavigation handleNavigation={handleNavigation} text={'Back'}/>
                 <ButtonNavigation handleNavigation={handleNavigation} text={'Next'}/>
-        </Box>
-    </>
+                </Box> */}
+
+        </>
     )
 }
--- itinerario-frontend/src/pages/PlanningPage.jsx
+++ itinerario-frontend/src/pages/PlanningPage.jsx
@@ -6,34 +6,64 @@ import LodgingMain from '../components/MainPlanningComponents/BodyPlanning/Lodgi
 import LogInForm from '../components/GenericComponents/LogInForm/LogInForm'
 import SignUpForm from '../components/GenericComponents/SignUpForm/SignUpForm'
 import SummaryMain from '../components/MainPlanningComponents/BodyPlanning/SummaryMain/SummaryMain'
-import { useState,useEffect } from 'react'
+import Box from '@mui/material/Box'
+import ButtonNavigation from '../components/GenericComponents/ButtonNavigation/ButtonNavigation'
+import { useState, useEffect } from 'react'
+
+
+
 
 export default function PlanningPage() {
-    const [stepperStatus,setStepperStatus ] = useState(0)
-    useEffect(()=>{
-        console.log(stepperStatus)
-    },[stepperStatus])
 
-    const handleNavigation = (textValue) =>{
-        if(textValue==="Back"){  
-            if(stepperStatus>0) setStepperStatus(stepperStatus - 1) 
-        }else {
-            if(stepperStatus<=3) setStepperStatus(stepperStatus + 1)    
-        } 
+    const [stepperStatus, setStepperStatus] = useState(0)
+    useEffect(() => {
+        console.log(stepperStatus)
+    }, [stepperStatus])
+
+    const handleNavigation = (textValue) => {
+        if (textValue === "Back") {
+            if (stepperStatus > 0) setStepperStatus(stepperStatus - 1)
+        } else {
+            if (stepperStatus <= 3) setStepperStatus(stepperStatus + 1)
+        }
     }
 
+    function renderButtons() {
+        return (
+            <Box sx={{ display: 'flex', justifyContent: 'space-around', my: "40px" }}>
+                {stepperStatus === 0 && <ButtonNavigation handleNavigation={handleNavigation} text={'Next'} />}
+                {stepperStatus > 0 && stepperStatus < 4 && <>
+                    <ButtonNavigation handleNavigation={handleNavigation} text={'Back'} />
+                    <ButtonNavigation handleNavigation={handleNavigation} text={'Next'} />
+                </>}
+                {stepperStatus === 4 && <>
+                    <ButtonNavigation handleNavigation={handleNavigation} text={'Back'} />
+                    <ButtonNavigation handleNavigation={handleNavigation} text={'Go to Summary'} bgColor={"#4BB449"} />
+                </>}
+            </Box>
+        )
+    }
 
     return (
+
         <div>
-            <HeaderPlanning stepperStatus={stepperStatus}/>
- {/*            
+            <HeaderPlanning stepperStatus={stepperStatus} />
+
+            {/*            
              <LogInForm />
              <SummaryMain /> 
-            <SignUpForm />*/}
-            <CalendarComponent handleNavigation={handleNavigation}/>
-            <FlightMain handleNavigation={handleNavigation}/>
-            <LodgingMain handleNavigation={handleNavigation}/>
-            <ActivityMain handleNavigation={handleNavigation} />
+             <SignUpForm />
+            */}
+
+
+            {/* <CalendarComponent /> */}
+            <FlightMain />
+            {/* <LodgingMain />
+            <ActivityMain /> */}
+
+            {renderButtons()}
+
         </div>
+
     )
 }
