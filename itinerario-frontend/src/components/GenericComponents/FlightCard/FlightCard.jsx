d4e16354cad52d79d81628e51dd9ce0674a9d14a
nextokoi <nextokoi.dev@gmail.com>
2023-12-17 13:55:52 +0000

    useRef in flightCard

--- itinerario-frontend/src/components/GenericComponents/FlightCard/FlightCard.jsx
+++ itinerario-frontend/src/components/GenericComponents/FlightCard/FlightCard.jsx
@@ -5,7 +5,7 @@ import Typography from '@mui/material/Typography';
 import { Divider } from '@mui/material';
 import AirplanemodeActiveIcon from '@mui/icons-material/AirplanemodeActive';
 import PropTypes from 'prop-types'
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import './FlightCard.css'
 
 export default function FlightCard({ data, date, onSelect, isSelected, airlines, origin, destination }) {
@@ -17,58 +17,65 @@ export default function FlightCard({ data, date, onSelect, isSelected, airlines,
         onSelect(data, !selected)
     }
 
-    const numAirlines = airlines.length
-    const randomAirline = Math.floor(Math.random() * numAirlines)
-    const airlineRandom = airlines[randomAirline]
+    const getRandomAirline = (airlines) => {
+        const numAirlines = airlines.length;
+        const randomAirlineIndex = Math.floor(Math.random() * numAirlines);
+        return airlines[randomAirlineIndex];
+    };
 
-    const randomNumberFlight = Math.floor(Math.random() * (98979 - 12501 + 1)) + 12501;
+    const getRandomNumberFlight = (min, max) => {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    };
 
-    function convertHoursToHoursAndMinutes(hours) {
+    const convertHoursToHoursAndMinutes = (hours) => {
         const wholeHours = Math.floor(hours);
         const decimalMinutes = (hours - wholeHours) * 60;
         const roundedMinutes = Math.round(decimalMinutes);
-    
+
         const hoursText = `${wholeHours} hr`;
         const minutesText = roundedMinutes > 0 ? ` ${roundedMinutes} min` : '';
-    
-        return `${hoursText}${minutesText}`;
-    }
-    
-    const durationInHours = data.duration / 60
-    const durationTwo = durationInHours.toFixed(2)
-    const durationHoursFloat = parseFloat(durationTwo)
-    const duration = convertHoursToHoursAndMinutes(durationHoursFloat)
 
-    function getRandom24HourTime() {
+        return `${hoursText}${minutesText}`;
+    };
+
+    const getRandom24HourTime = () => {
         const randomHours = Math.floor(Math.random() * 24);
         const randomMinutes = Math.floor(Math.random() * 60);
-    
+
         const formattedTime = `${String(randomHours).padStart(2, '0')}:${String(randomMinutes).padStart(2, '0')}`;
         return formattedTime;
-    }
+    };
+
+    const getFormattedDuration = (duration) => {
+        const durationInHours = duration / 60;
+        const durationHoursFloat = parseFloat(durationInHours.toFixed(2));
+        return convertHoursToHoursAndMinutes(durationHoursFloat);
+    };
+
+    const duration = getFormattedDuration(data.duration)
+    const airlineRandom = useRef(getRandomAirline(airlines))
+    const randomNumberFlight = useRef(getRandomNumberFlight(12501, 98979))
+    const randomTime = useRef(getRandom24HourTime())
     
-    const randomTime = getRandom24HourTime();
-
-
     return (
         <Card sx={{ py: 3, px: 1, minWidth: 300, maxWidth: 500, cursor: 'pointer', "&:hover": { border: isSelected ? '2px solid #1976d2' : '2px solid green' } }} className={isSelected ? 'selected' : 'unselected'} onClick={handleClick}>
             <CardContent>
                 <Box sx={{display: 'flex', justifyContent: 'space-between', alignItems: 'center'}}>
                     <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', gap: '5px'}}>
-                        <img src={airlineRandom.imageLogo} alt="" width={30}/>
+                        <img src={airlineRandom.current.imageLogo} alt="" width={30}/>
                         <Typography variant='body2'>
-                            {airlineRandom.name}
+                            {airlineRandom.current.name}
                         </ Typography>
                     </Box>
                     <Box sx={{display: 'flex', flexDirection: 'column', alignItems: 'end'}}>
                         <Typography sx={{fontWeight: 'bold'}}>
-                            {`${airlineRandom.code}${randomNumberFlight}`} 
+                            {`${airlineRandom.current.code}${randomNumberFlight.current}`} 
                         </Typography>
                         <Typography sx={{fontWeight: 'bold'}}>
                             {date}
                         </Typography>
                         <Typography sx={{fontWeight: 'bold'}}>
-                            {randomTime}
+                            {randomTime.current}
                         </Typography>
                     </Box>
                 </Box>
