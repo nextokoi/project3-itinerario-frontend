125844bacc77d55e024ab53043ab5189b7786772
nextokoi <nextokoi.dev@gmail.com>
2023-12-19 20:03:38 +0000

    conection attempted for deploy

--- itinerario-frontend/src/services/config.js
+++ itinerario-frontend/src/services/config.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: "project3-itinerario-backend-production.up.railway.app",
 })
 
 export default api
